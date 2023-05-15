13c2d9b3c632a7ac53468604c34b130bdb9635ce
Shubham Shrivastava <shubhamshrivastava2000@gmail.com>
2023-05-16 01:33:16 +0530

    zIndex fix

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -303,6 +303,7 @@ export default function Home() {
                         >
                           {comment.postedBy.name}{" "}
                         </span>
+
                         <span className="commentText">{comment.comment}</span>
                       </p>
                     );
