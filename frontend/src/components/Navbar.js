01f9fc96fe73a6209ca9d8413d653e778b788462
UMANG SADHWANI <umangsadhwani@UMANGs-MacBook-Air.local>
2023-05-24 15:38:16 +0530

    responsive navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -7,6 +7,45 @@ import { LoginContext } from "../context/LoginContext";
 export default function Navbar({ login }) {
   const { setModalOpen } = useContext(LoginContext);
 
+  const loginStatusMobile = () => {
+    const token = localStorage.getItem("jwt");
+
+    if (login || token) {
+      return [
+        <>
+          <Link to="/">
+          <span class="material-symbols-outlined">
+          home
+          </span>
+          </Link>
+          <Link to="/profile">
+            <li><span class="material-symbols-outlined">account_circle</span></li>
+          </Link>
+          <Link to="/createPost">
+            <li><span class="material-symbols-outlined">add_a_photo</span></li>
+          </Link>
+
+          <Link to={""}>
+            <li  onClick={() => setModalOpen(true)}>
+            <span class="material-symbols-outlined">logout</span>
+            </li>
+          </Link>
+        </>,
+      ];
+    } else {
+      return [
+        <>
+          <Link to="/signup">
+            <li>SignUp</li>
+          </Link>
+          <Link to="/signin">
+            <li>SignIn</li>
+          </Link>
+        </>,
+      ];
+    }
+  };
+
   const loginStatus = () => {
     const token = localStorage.getItem("jwt");
 
@@ -43,8 +82,9 @@ export default function Navbar({ login }) {
   return (
     <>
       <div className="navbar">
-        <img src={rgpvlogo} width="40px" style={logoStyle} />
+        <img id="rgpv-logo" src={rgpvlogo} width="40px" style={logoStyle} />
         <ul className="nav-menu">{loginStatus()}</ul>
+        <ul className="nav-mobile">{loginStatusMobile()}</ul>
       </div>
     </>
   );
